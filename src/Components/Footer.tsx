1bc3003ca74bda43c45132e4e7b445a277e1072f
MontaLipsane <monta.lipsane@takeda.com>
2021-05-29 13:50:12 +0300

    clickable e-mail and phone number

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,15 +1,19 @@
 // import { header } from '../CSS/header.css';
 import "../CSS/header.css";
 
-
 function Footer() {
-    return (
-        <div>
-            <div className="footer">
-              Sauleja tel.nr. +371 26514044 info@sauleja.lv
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="footer">
+        <p>
+          <b>Izdevniecība "Sauleja"</b> <br></br>
+          tel.nr. <a href="tel:37126514044">+371 26514044 </a>
+          <br></br>
+          e-pasts <a href="mailto:info@sauleja.lv">info@sauleja.lv</a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
