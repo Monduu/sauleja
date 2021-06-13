bdc35b6baf380a8be71842bf33a3547387dd2d72
r6q <17678057+r6q@users.noreply.github.com>
2021-06-13 18:34:57 +0300

    ✨ add BookCard component

---
+++ src/Components/BookCard.tsx
@@ -0,0 +1,20 @@
+import Col from "react-bootstrap/Col";
+import React, {FC} from "react";
+
+interface Props {
+  bookKey: string,
+}
+
+export const BookCard: FC<Props> = ({bookKey}) => {
+  return (
+    <Col lg={4} sm={12}>
+      <div className="s-book-card">
+        <img
+          src={`/images/${bookKey}/main.jpg`}
+          className="d-inline-block align-top pointer thumbnail s-fill-width"
+          alt={bookKey}
+        />
+      </div>
+    </Col>
+  )
+}
