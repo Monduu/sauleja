af289191a1c74eb22fe03cdc60f8b43612fb473d
r6q <17678057+r6q@users.noreply.github.com>
2021-06-13 19:15:31 +0300

    ✨ add base layout to BookView

--- src/Views/BookView.tsx
+++ src/Views/BookView.tsx
@@ -1,7 +1,34 @@
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import "../CSS/home.css";
+import Row from "react-bootstrap/Row";
+import { useParams } from "react-router-dom";
+import {BooksMetadata} from "../data/BooksMetadata";
+
+interface ParamTypes {
+  bookName: string
+}
+
 export const BookView = () => {
+  const {bookName} = useParams<ParamTypes>();
+  const book = BooksMetadata.get(bookName)! // TODO redirect to 404
+
   return (
     <div>
-      TODO add book view
+      <Container>
+        <Row>
+          <Col sm={12} md={6} lg={5} >
+            <img src={`/images/${bookName}/main.jpg`}
+                 className="d-inline-block pointer s-fill-width"
+                 alt={bookName}/>
+          </Col>
+          <Col sm={12} md={6} lg={7}>
+            <div>
+              {book.description}
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
--- src/data/BooksMetadata.ts
+++ src/data/BooksMetadata.ts
@@ -5,7 +5,11 @@ export interface BookData {
 
 export const BooksMetadata: Map<string, BookData> = new Map<string, BookData>([
   ['bibele', {
-    description: 'TODO1',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut ' +
+      'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ' +
+      'aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore' +
+      ' eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt' +
+      ' mollit anim id est laborum.',
     numOfImages: 1,
   }],
   ['dieniska_gaisma', {
